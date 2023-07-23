8abd2584b1e3ee0943538d1086d5a96a590c0e71
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2023-07-23 18:54:27 -0500

    Validations

--- src/main.ts
+++ src/main.ts
@@ -16,10 +16,23 @@ let posts: Post[] = [];
 
 app.post('/posts', (req, res) => {
   const { id, title, content } = req.body;
-  const newPost: Post = { id, title, content };
-  posts.push(newPost);
-  res.json(newPost);
+
+  // Verificar que se proporcionen todos los datos requeridos
+  if (!id || !title || !content) {
+    res.status(400).json({ error: 'Faltan datos requeridos para crear el post.' });
+    return;
+  }
+
+  // Intentar crear el nuevo post
+  try {
+    const newPost: Post = { id, title, content };
+    posts.push(newPost);
+    res.status(201).json(newPost); // Envía el post recién creado como respuesta en formato JSON
+  } catch (error) {
+    res.status(500).json({ error: 'Ocurrió un error al intentar crear el post.' });
+  }
 });
+
 
 app.get('/posts', (req, res) => {
   res.json(posts);
@@ -27,9 +40,23 @@ app.get('/posts', (req, res) => {
 
 app.delete('/posts/:id', (req, res) => {
   const postId = parseInt(req.params.id, 10);
-  posts = posts.filter((post) => post.id !== postId);
+
+  // Buscar el post en el arreglo de posts
+  const postIndex = posts.findIndex((post) => post.id === postId);
+
+  // Si el post no existe, responder con un estado HTTP 404 (Not Found)
+  if (postIndex === -1) {
+    res.status(404).json({ error: 'El post no fue encontrado.' });
+    return;
+  }
+
+  // Eliminar el post del arreglo
+  posts.splice(postIndex, 1);
+
+  // Responder con un estado HTTP 200 (OK) para indicar que el post fue eliminado correctamente
   res.sendStatus(200);
 });
+
 
 
 app.listen(PORT, () => {
