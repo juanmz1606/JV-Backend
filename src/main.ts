d77deadb18696786aec473eabfa0e81c5dcee3f4
juanmz1606 <juan.1701920466@ucaldas.edu.co>
2023-08-21 12:38:08 -0500

    Carga hasta la obtención de las entidades Post

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import cors from 'cors'; // Importa el paquete cors
 
 const app = express();
 const PORT = 3000;
 
+app.use(cors()); // Habilita CORS para todas las solicitudes
 app.use(express.json());
 
 // Aquí implementarás los endpoints (Create, Delete, Read) para la entidad "Post"
